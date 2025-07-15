5b1ef12b922b6aaffd4bee1abbccefe1f56394c4
Your Name <urielparavi75@gmail.com>
2025-07-16 01:11:57 +0300

    249. Adding & Using Express (+ Types)

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(req.method);
+  res.json({ message: 'Hello World' });
+});
+
+app.listen(3000);
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,7 @@
-console.log('hello world');
-export {};
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    console.log(req.method);
+    res.json({ message: 'Hello World' });
+});
+app.listen(3000);
